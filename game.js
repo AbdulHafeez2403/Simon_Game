606a1ecee9ada3752174e1b41e5844542715bcd8
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2023-07-09 20:03:09 +0530

    animation and sound on click

--- game.js
+++ game.js
@@ -7,19 +7,36 @@ function nextSequence() {
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
-    // step 3
     // $("#"+randomChosenColor);
     $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 //paly audio for selected button
-    var audio = new Audio("sounds/"+randomChosenColor+".mp3");
-    audio.play();
-   
+    // var audio = new Audio("sounds/"+randomChosenColor+".mp3");
+    // audio.play();
+   playSound(randomChosenColor);
 }
 
-//step 4
+
+function playSound(name) {
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+
+
+
 $(".btn").click(function(){
     var userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
     // console.log(userClickedPattern);
-    nextSequence();
+    // nextSequence();
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
+
+//step 6
+function animatePress(currentColor) {
+    $("#"+currentColor).addClass("pressed");
+
+    setTimeout(() => {
+        $("#"+currentColor).removeClass("pressed");
+    }, 100);
+}
